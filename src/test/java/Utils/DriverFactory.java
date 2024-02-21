34109c770ecc95507a3655e4ed8edc361ee2ca5a
Maciej Zacharski <johnzpolski@gmail.com>
2024-02-21 11:51:11 +0100

    Driver manager config

--- src/test/java/Utils/DriverFactory.java
+++ src/test/java/Utils/DriverFactory.java
@@ -3,12 +3,20 @@ package Utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
 
     public static WebDriver getDriver() {
         WebDriverManager.chromedriver().setup();
+        final var options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("start-maximized");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("window-size=1920,1920");
         return new ChromeDriver();
 
     }
